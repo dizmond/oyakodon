4eceb7b9871fcc69f84af04a7bbca38008c9635c
lamngo13 <lamngo@live.unc.edu>
2022-11-18 15:12:12 -0500

    YAY MADE AMERICAN UNITS LGO

--- pmstuff.py
+++ pmstuff.py
@@ -10,13 +10,34 @@ def list_attributes(tpkm):
     #tbh I want tpkm to be a string
     pkm = pb.pokemon(tpkm)
     print("name: " + pkm.name)
-    print("abilities: " + str(pkm.abilities[0].name))
-    #todo figure out how this works!! ^
+    print("abilities: ")
+    num_abilities = len(pkm.abilities)
+    for i in range(0,num_abilities,1):
+        print(pkm.abilities[i].ability.name)
+        #print(pkm.abilities[i].ability.name)
+    print("height: " + str(decimetres_to_american(pkm.height)))
+    print("weight: " + str(hecto_to_lb(pkm.weight)))
+    num_types = len(pkm.types)
+    print("types:")
+    for i in range(0,num_types,1):
+        print(pkm.types[i].type.name)
+    #past types??
+
+def hecto_to_lb(hec):
+    return int(round(hec / 4.5359237,0))
+
+def decimetres_to_american(decimetres):
+    total_num_inches = int(round(decimetres * 3.93700787402,0))
+    extra_inches = total_num_inches % 12
+    num_feet = int(round((total_num_inches - extra_inches) / 12))
+    return(str(num_feet)+"\'"+str(extra_inches)+"\"")
 
 
-charmander = pb.pokemon('charmander')  # Quick lookup.
-print("charmander height: " + str(charmander.height))
-print("\n")
+
+#charmander = pb.pokemon('charmander')  # Quick lookup.
+#print("charmander height: " + str(charmander.height))
+#print("\n")
 
 
 list_attributes('piplup')
+list_attributes('empoleon')
