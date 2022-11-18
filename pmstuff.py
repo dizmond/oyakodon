a4854a0ed3bc64727fad707316cadcb78ac5c1bc
lamngo13 <lamngo@live.unc.edu>
2022-11-18 14:09:07 -0500

    list_attributes for pmstuff

--- pmstuff.py
+++ pmstuff.py
@@ -1,3 +1,22 @@
 import pokebase as pb
+#todo???
+#from pokebase import *
+
+#from pokebase import cache
+#cache.API_CACHE
+
+#easy way to list all the stuff we want!!
+def list_attributes(tpkm):
+    #tbh I want tpkm to be a string
+    pkm = pb.pokemon(tpkm)
+    print("name: " + pkm.name)
+    #print("abilities: " + str(pkm.abilities[0][0]))
+    #todo figure out how this works!! ^
+
+
 charmander = pb.pokemon('charmander')  # Quick lookup.
 print("charmander height: " + str(charmander.height))
+print("\n")
+
+
+list_attributes('piplup')
