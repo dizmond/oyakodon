2ef6d45a24c54c5acb20f31286a12b086dc9f4ec
lamngo13 <lamngo@live.unc.edu>
2022-12-16 16:54:02 -0500

    chknnegg

--- webby/tempmain.js
+++ webby/tempmain.js
@@ -1,13 +1,47 @@
 const Pokedex = require("pokeapi-js-wrapper")
 const P = new Pokedex.Pokedex()
-(async () => {
-    const golduck = await P.getPokemonByName("golduck")
-    console.log(golduck)
-  })()
 
-document.getElementById("testy").innerHTML = golduck;
+var pkmweight = 0;
+var pkmheight = 0;
+//pkm = pb.pokemon(tpkm)
+
+P.getPokemonByName("empoleon").then(function(response) {
+    //console.log(response[0])
+    //console.log(response[1])
+    console.log(response['weight'])
+    console.log(response['height'])
+    console.log(response['abilities'])
+})
+// console.log(pkm)
+// const interval = {
+//     offset: 24,
+//     limit: 10,
+//   }
+//   P.getPokemonsList(interval).then(function(response) {
+//     console.log(response)
+//     console.log(response[1][1])
+//   })
+
+// (async () => {
+//     const pkms = await P.getPokemonByName("golduck")
+//     console.log(pkms)
+//   })()
+
+// P.getPokemonByName("eevee")
+//   .then(function(response) {
+//     console.log(response)
+//   })
+
+//var golduck = P.getPokemonByName("golduck");
+//console.log(golduck)
+
+//golduck = "asdfasdf"
 
 
+//THIS ALONE WORKS
+//var sparky = "sparky"
+//document.getElementById("testy").innerHTML = sparky;
+//document.getElementById("testy").innerHTML = golduck;
 
 
 //<script src="https://unpkg.com/pokeapi-js-wrapper/dist/index.js"></script>
