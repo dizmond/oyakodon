dbc9788383108fe39689e15799246d8fc1b18e96
lamngo13 <lamngo@live.unc.edu>
2022-12-16 10:59:25 -0500

    gitignored node_modules yay

--- webby/tempmain.js
+++ webby/tempmain.js
@@ -1,1 +1,13 @@
-document.getElementById("testy").innerHTML = "yahyeet";
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex()
+(async () => {
+    const golduck = await P.getPokemonByName("golduck")
+    console.log(golduck)
+  })()
+
+document.getElementById("testy").innerHTML = golduck;
+
+
+
+
+//<script src="https://unpkg.com/pokeapi-js-wrapper/dist/index.js"></script>
