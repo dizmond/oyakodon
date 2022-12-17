69586f7bede4ea649dbd2f9d96f8b898652d874f
lamngo13 <lamngo@live.unc.edu>
2022-12-17 13:24:21 -0500

    4sarai

---
+++ newapi.js
@@ -0,0 +1,4 @@
+import Pokedex from 'pokedex-promise-v2';
+const P = new Pokedex();
+
+console.log("ayyo")
--- webby/tempmain.js
+++ webby/tempmain.js
@@ -1,18 +1,42 @@
-const Pokedex = require("pokeapi-js-wrapper")
+
+//@jest-environment jsdom;
+
+
+//const Pokedex = require("pokeapi-js-wrapper")
+//const P = new Pokedex.Pokedex()
+import Pokedex from "https://unpkg.com/pokeapi-js-wrapper/dist/index.js";
 const P = new Pokedex.Pokedex()
 
-var pkmweight = 0;
-var pkmheight = 0;
+var pkmweight = 420;
+var pkmheight = 421;
+console.log(pkmweight)
+console.log(pkmheight)
 //pkm = pb.pokemon(tpkm)
 
-P.getPokemonByName("empoleon").then(function(response) {
-    //console.log(response[0])
+///P.getPokemonByName("empoleon").then(function(response) {
+    //console.log(response)
+    // console.log("Start")
+    // console.log(response['base_experience'])   //239
+    // console.log(response['weight'])   //845
+    // console.log(response['height']) //17
+    // console.log(response['abilities'])
+
+    // console.log("end")
+    ///pkmweight = response['weight'];
+    ///pkmheight = response['height'];
     //console.log(response[1])
-    console.log(response['weight'])
-    console.log(response['height'])
-    console.log(response['abilities'])
-})
-// console.log(pkm)
+    //console.log(response['weight'])
+    //////pkmweight = response['weight']
+    //////pkmheight = response['height']
+    //console.log(response['height'])
+    //console.log(response['abilities'])
+///})
+//console.log(pkmweight)
+//console.log(pkmheight)
+
+
+//BIGEND
+
 // const interval = {
 //     offset: 24,
 //     limit: 10,
@@ -41,7 +65,8 @@ P.getPokemonByName("empoleon").then(function(response) {
 //THIS ALONE WORKS
 //var sparky = "sparky"
 //document.getElementById("testy").innerHTML = sparky;
-//document.getElementById("testy").innerHTML = golduck;
+//document.getElementById("testy").innerHTML = 6969;
+//document.getElementById("testy2").innerHTML = 420;
 
 
 //<script src="https://unpkg.com/pokeapi-js-wrapper/dist/index.js"></script>
