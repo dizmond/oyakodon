59d7d63cf474ef9081ce890ab5002768495a21e3
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2023-01-03 14:04:06 -0500

    updated scope in html appjs

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ const CLIENT_ID = '71a6034a2d9040ebb2d4c06d40043aff';
 const REDIRECT_URI = "http://localhost:3000";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
+const scopes = ['user-read-currently-playing', 'user-read-recently-played', 'user-read-playback-state']
 
 
 
@@ -169,7 +170,7 @@ function App() {
         {!token ?
           <>
             <h2>Please login</h2>
-            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>Login to Spotify</a>
           </>
           : <>
             <button onClick={logout}>Logout</button>
